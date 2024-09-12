55516375ff58b36aebbfd94b60d8952067831d79
rexxarthas <148492594+rexxarthas@users.noreply.github.com>
2024-09-12 12:08:36 -0300

    Update Postagem.php

--- Postagem.php
+++ Postagem.php
@@ -1,39 +1,58 @@
 <?php
 
-class PostagemDAO {
+class Postagem {
     public $pensamento;
     public $figura_nome;
     public $id_perfil;
     public $id_postagem;
 
     public function cadastrar_figura() {
-        $objeto = new Conexao();
-        $SQL = "INSERT INTO Postagem (figura_nome, id_perfil )
-               VALUES ('$this->pensamento', '$this->id_perfil' ) ;";
-        $objeto->set("sql", $SQL);
-        echo $SQL;
-        $objeto->query();
-        return "Cadastrado com Sucesso";
+        $objeto = new PostagemDAO(); // Corrigido o erro de digitação
+        $objeto->set("id_perfil", $this->id_perfil);
+        $nomearquivo = $this->imagem;
+        $objeto->set("figura_nome", $nomearquivo["name"]);
+
+        
+        return $objeto->cadastrar();
     }
-    
+
+
     public function excluir() {
-        $objeto = new Conexao();
-        $SQL = "DELETE FROM pagprincipal WHERE pensamento = $this->pensamento;";
-        $objeto->set("sql", $SQL);
-        $objeto->query();
-        return "Excluido com Sucesso";
+        $objeto = new PostagemDAO();
+        $objeto->set("id_postagem", $this->id_postagem);
+        return $objeto->excluir();
+    }
+
+    public function consultar() {
+        $objeto = new PostagemDAO();
+        $objeto->set("id_perfil", $this->id_perfil);
+
+        return $objeto->consultar();
     }
 
     public function set($prop, $value) {
         $this->$prop = $value;
     }
-    
-    public function consultar(){
-        $objeto = new Conexao();
-        $SQL = "SELECT * "
-                . "FROM Postagem";
-        $objeto->set("sql", $SQL);
-        return $objeto->query();
+
+    public function gravarimagem(){
+        try {
+            $pasta_dir = "fotos_$this->id_perfil/";
+            if(!file_exists($pasta_dir)) {
+                mkdir ($pasta_dir);
+            }
+            $arquivo = $this->imagem;
+            $nomearquivo = $arquivo["name"]; //nome da imagem
+
+            if ($arquivo["type"] == "image/jpeg" || $arquivo["type"] == "image/png") { //filtrar tipo de imagem
+                move_uploaded_file($arquivo["tmp_name"], $pasta_dir . $nomearquivo);
+            }else{
+                return "tipo permitido de imagem: JPG ou PNG";
+            }
+            return "Upload da foto realizado com sucesso";
+        } catch (exception $ex) {
+            return $ex;
+        }
     }
 
 }
+?>
